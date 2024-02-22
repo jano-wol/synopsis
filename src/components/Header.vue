d196bea2b95b9408bc991bc51fd002a9c661094a
anwolosz <anwolosz@gmail.com>
2024-02-22 23:46:30 +0100

    Header fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,6 +19,11 @@
                         </router-link>
                     </li>
                     <li class="nav-item">
+                        <router-link :to="{ name: 'index' }" :class="$route.name == 'index' ? 'active' : ''"
+                            class="nav-link" aria-current="page">Tartalomjegyzék
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
                         <router-link :to="{ name: 'description' }" :class="$route.name == 'description' ? 'active' : ''"
                             class="nav-link" aria-current="page">Leírás
                         </router-link>
@@ -38,41 +43,3 @@
         </div>
     </nav>
 </template>
-
-
-
-<!-- <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'synopsis' }" :class="$route.name == 'synopsis' ? 'active' : ''"
-                            class="nav-link" aria-current="page">Szinopszis
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'description' }" :class="$route.name == 'description' ? 'active' : ''"
-                            class="nav-link" aria-current="page">Leírás
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'index' }" :class="$route.name == 'index' ? 'active' : ''"
-                            class="nav-link" aria-current="page">Tartalomjegyzék
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'details' }" :class="$route.name == 'details' ? 'active' : ''"
-                            class="nav-link" aria-current="page">Részletek
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link :to="{ name: 'developers' }" :class="$route.name == 'developers' ? 'active' : ''"
-                            class="nav-link" aria-current="page">Fejlesztőknek
-                        </router-link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</template> -->
