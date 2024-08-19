2f2a66fc991611b0dd78d9953786c23a1ab9be3f
anwolosz <anwolosz@gmail.com>
2024-08-19 18:04:56 +0200

    target blank

--- src/components/en/DevelopmentEn.vue
+++ src/components/en/DevelopmentEn.vue
@@ -1,11 +1,11 @@
 <template>
     <p>Development takes place on GitHub. The repo can be found on <a
-            href="https://github.com/anwolosz/synopsis">this</a> link.
+            href="https://github.com/anwolosz/synopsis" target="_blank">this</a> link.
     </p>
     <p>The .json databases of the Synopsis are available at the links below.</p>
     <ul>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_szit.json">SZIT</a>
+        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_szit.json" target="_blank">SZIT</a>
         </li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_esv.json">ESV</a></li>
+        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_esv.json" target="_blank">ESV</a></li>
     </ul>
 </template>
--- src/components/hu/DevelopmentHu.vue
+++ src/components/hu/DevelopmentHu.vue
@@ -1,10 +1,10 @@
 <template>
-    <p>A fejlesztés GitHubon zajlik. A repo <a href="https://github.com/anwolosz/synopsis">ezen</a> a linken érhető el.
+    <p>A fejlesztés GitHubon zajlik. A repo <a href="https://github.com/anwolosz/synopsis" target="_blank">ezen</a> a linken érhető el.
     </p>
     <p>A Szinopszis vázát képező .json adatbázisok az alábbi linkeken érhetők el.</p>
     <ul>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_szit.json">SZIT</a>
+        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_szit.json" target="_blank">SZIT</a>
         </li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_esv.json">ESV</a></li>
+        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/synopsis_esv.json" target="_blank">ESV</a></li>
     </ul>
 </template>
