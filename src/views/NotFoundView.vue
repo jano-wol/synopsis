cd6d94bf28d408f0d9dc1b8e0317016ca62858c7
anwolosz <anwolosz@gmail.com>
2024-08-20 00:22:56 +0200

    Not found page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,18 @@
+<script lang="ts">
+import { useSynopsisStore } from "@/stores/SynopsisStore"
+
+export default {
+    data() {
+        return {
+            synopsisStore: useSynopsisStore()
+        };
+    }
+}
+
+</script>
+
 <template>
     <div class="container">
-        <h1 class="text-center"> 404 Not found</h1>
+        <h1 class="text-center">4<img src="/favicon.svg" alt="Szinopszis" width="22" height="22">4 {{ synopsisStore.translation.notFound.notFound}}</h1>
     </div>
 </template>
