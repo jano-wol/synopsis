d3f0a475b3f9963b5aef2c616a230280b928c305
anwolosz <anwolosz@gmail.com>
2024-08-21 16:29:00 +0200

    CI restructre

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 SCRIPT_FOLDER=$(dirname "${0}") 
-WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../../")
+WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../")
 JSON_FOLDER="$WORKSPACE_FOLDER/src/assets/"
 python3 "$SCRIPT_FOLDER"/json_test.py "$JSON_FOLDER"
+echo "All tests passed"
