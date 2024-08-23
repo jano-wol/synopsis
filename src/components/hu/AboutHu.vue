fbb61f9e9d1ea84b6ab6cb5b8fff752fd2d6c65f
anwolosz <anwolosz@gmail.com>
2024-08-24 00:11:15 +0200

    Refactor

--- src/components/hu/AboutHu.vue
+++ src/components/hu/AboutHu.vue
@@ -26,23 +26,23 @@ export default {
             <router-link    :to="{ name: 'index', params: { lang: synopsisStore.language, translation: synopsisStore.translation } }">tartalomjegyzékben</router-link>
             vastag kiemeléssel szerepelnek
             a hozzá tartozó idézetek. A törzsszöveg a teljes evangéliumot tartalmazza, ismétlődések nélkül, a versek
-            eredeti sorrendjében. Minden alszekcióban egy evangélistához, legfeljebb egy törzsszöveg található.
+            eredeti sorrendjében. Minden szekcióban egy evangélistához, legfeljebb egy törzsszöveg található.
         </li>
         <li>A párhuzamos szöveg szürke kerettel és árnyék nélkül jelenik meg.</li>
     </ul>
 
     <p>
-        Az alszekciók címe mellett a következő gombok találhatók.
+        Az szekciók címe mellett a következő gombok találhatók.
     </p>
     <ul>
         <li><i class="bi bi-arrow-up-right-square"></i> vagy <i class="bi bi-arrow-down-left-square"></i> : <br> A
             megnyitás külön oldalon ( <i class="bi bi-arrow-up-right-square"></i> ) gomb megnyomásával az adott
-            alszekció megnyitható egy különálló, saját aloldalon. <br> A megnyitás az eredeti helyen ( <i
-                class="bi bi-arrow-down-left-square"></i> ) gomb megnyomásával az adott alszekció elérhető a
+            szekció megnyitható egy különálló, saját aloldalon. <br> A megnyitás az eredeti helyen ( <i
+                class="bi bi-arrow-down-left-square"></i> ) gomb megnyomásával az adott szekció elérhető a
             Szinopszis egész szövegében elhelyezve.
         </li>
         <li><i class="bi bi-link-45deg"></i> : A másolás gomb megnyomásával vágólapra kerül a link, ami az adott
-            alszekcióra mutat.</li>
+            szekcióra mutat.</li>
     </ul>
 
     <p>
@@ -51,11 +51,11 @@ export default {
     <ul>
         <li><i class="bi bi-compass"></i> : Az átirányítás gombra kattintva az adott párhuzamos szöveg
             törzsszevegének
-            az alszekciója érhető el a Szinopszis egész szövegében elhelyezve.
+            az szekciója érhető el a Szinopszis egész szövegében elhelyezve.
         </li>
         <li><i class="bi bi-arrow-up"></i> és <i class="bi bi-arrow-down"></i> : A navigációs gombokra kattintva az
             adott törzsszöveget megelőző ( <i class="bi bi-arrow-up"></i> ), illetve követő ( <i
-                class="bi bi-arrow-down"></i> ) törzsszöveg alszekciója érhető el a Szinopszis egész
+                class="bi bi-arrow-down"></i> ) törzsszöveg szekciója érhető el a Szinopszis egész
             szövegében elhelyezve.
         </li>
 
