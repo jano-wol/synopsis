fafb8a76169e0e7405e00ffc061cb0a95d03072a
anwolosz <anwolosz@gmail.com>
2024-09-02 19:01:53 +0200

    json scheme test

--- tests/json_test.py
+++ tests/json_test.py
@@ -2,8 +2,22 @@ import json
 import os
 import sys
 
+def blank_values(json_element):
+    if isinstance(json_element, dict):
+        return {k: blank_values(v) for k, v in json_element.items()}
+    elif isinstance(json_element, list):
+        return [blank_values(item) for item in json_element]
+    else:
+        return None
+
+blank_json = None
+blank_json_file_path = sys.argv[2] 
+with open(blank_json_file_path, 'r') as file:
+    blank_json = json.load(file)
+
 json_folder = sys.argv[1]
 json_files_found = False
+
 for file in os.listdir(os.fsencode(json_folder)):
     filename = os.fsdecode(file)
     if filename.endswith(".json"):
@@ -13,8 +27,13 @@ for file in os.listdir(os.fsencode(json_folder)):
                 json_files_found = True
                 json_loaded = json.load(f)
             except ValueError as e:
-               print(f'Invalid json= {file_to_check}. error={e}')
-               sys.exit(1)
+                print(f'Invalid json= {file_to_check}. error={e}')
+                sys.exit(1)
+            json_loaded = blank_values(json_loaded)
+            if json_loaded != blank_json:
+                is_fitting_scheme = False
+                print(f'Invalid json= {file_to_check}. error=Not fitting scheme of blank json.({blank_json_file_path})')
+                sys.exit(1)
 if json_files_found is False:
     print(f'No json files were found in folder={json_folder}')
     sys.exit(1)
--- tests/test.sh
+++ tests/test.sh
@@ -3,5 +3,6 @@ set -ex
 SCRIPT_FOLDER=$(dirname "${0}") 
 WORKSPACE_FOLDER=$(readlink -e "${SCRIPT_FOLDER}/../")
 TRANSLATION_FOLDER="$WORKSPACE_FOLDER/src/assets/translations/"
-python3 "$SCRIPT_FOLDER"/json_test.py "$TRANSLATION_FOLDER"
+BLANK_JSON="${SCRIPT_FOLDER}/synopsis_blank.json"
+python3 "$SCRIPT_FOLDER"/json_test.py "$TRANSLATION_FOLDER" "$BLANK_JSON"
 echo "All tests passed"
