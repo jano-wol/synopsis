cfe9fac26fa9ca48b55c478b243a0221b15ff5de
anwolosz <anwolosz@gmail.com>
2024-09-08 09:25:13 +0200

    Refactor sources

--- src/views/SourcesView.vue
+++ src/views/SourcesView.vue
@@ -21,6 +21,28 @@ export default {
 <template>
     <div class="container">
         <h1 class="text-center">{{ synopsisStore.currentDictionary.menu.sources }}</h1>
+        <ul>
+            <li>
+                Biblia,
+                <a href="https://szentistvantarsulat.hu/">Szent István Társulat</a>,
+                <a href="https://szentiras.hu/api">szentiras.hu API</a>
+            </li>
+            <li>
+                Bible,
+                <a href="https://www.esv.org/">ESV</a>,
+                <a href="https://api.esv.org/">esv.org API</a>
+            </li>
+            <li>
+                Szinopszis – Máté, Márk, Lukács és János evangéliumának párhuzamos szövege,
+                <a href="https://kalvinkiado.hu/">Kálvin Kiadó</a>
+                , Budapest, 1994, ISBN: 9633005922
+            </li>
+            <li>
+                Synopsis Quattuor Evangeliorum, Kurt Aland,
+                <a href="https://www.die-bibel.de/">Deutsche Bibelgesellschaft</a>
+                , Stuttgart, 2005, ISBN: 9783438051301
+            </li>
+        </ul>
         <SourcesHu v-if="synopsisStore.currentLanguage === 'hu'" />
         <SourcesEn v-if="synopsisStore.currentLanguage !== 'hu'" />
     </div>
