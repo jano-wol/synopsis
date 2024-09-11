39ee2824ef052862653f2aa5d5e4ec8cbb3a3bd8
Andrzej Polak <jjjkamu@gmail.com>
2024-09-11 21:46:49 +0200

    Cleanup

--- tests/title_test.py
+++ tests/title_test.py
@@ -6,13 +6,17 @@ from bs4 import BeautifulSoup
 tested_translations = ['ESV', 'KG', 'KNB', 'SZIT', 'UF']
 
 
+def get_change_table_path(components_folder, translation):
+    return components_folder + 'TitleChangeTable' + translation + '.vue'
+
+
 def getIdAndTitle(table_element):
     a, b = table_element.split(' ', 1)
     return a, b
 
 
-def read_overwritten_titles(translation, components_folder):
-    path = components_folder + 'TitleChangeTable' + translation + '.vue'
+def read_overwritten_titles(components_folder, translation):
+    path = get_change_table_path(components_folder, translation)
     if os.path.isfile(path) is False:
         return None
     with open(path, 'r', encoding='utf-8') as file:
@@ -35,18 +39,18 @@ def read_overwritten_titles(translation, components_folder):
     overwritten_titles = {}
     for row in table_data:
         if len(row) != 2:
-            print(f'Table row contains other than two elements. path={path} row={row}')
+            print(f'Change table row contains other than two elements. path={path} row={row}')
             sys.exit(1)
         id1, title1 = getIdAndTitle(row[0])
         id2, title2 = getIdAndTitle(row[1])
         if id1 != id2:
-            print(f'Table row contains different ids. id1={id1} id2={id2} path={path} row={row}')
+            print(f'Change table row contains different ids. id1={id1} id2={id2} path={path} row={row}')
             sys.exit(1)
         if title1 == title2:
-            print(f'Table row contains identical titles (superfluous row). title={title1} path={path} row={row}')
+            print(f'Change table row contains identical titles (superfluous row). title={title1} path={path} row={row}')
             sys.exit(1)
         if id1 in overwritten_titles:
-            print(f'Same id is presented in multiple rows. id={id1} path={path}')
+            print(f'Change table contains multiple rows with the same id. id={id1} path={path}')
             sys.exit(1)
         overwritten_titles[id1] = [title1, title2]
     return overwritten_titles
@@ -58,12 +62,30 @@ def check_overwritten(default_titles, overwritten_titles, translation):
     for id1, title_pair in overwritten_titles.items():
         title1, title2 = title_pair
         if id1 not in default_titles:
-            print(f'overwritten id is not found in default. overwritten_id={id1} translation={translation}')
+            print(f'Change table contains unexpected id. id={id1} translation={translation}')
             sys.exit(1)
         default_title = default_titles[id1]
         if default_title != title1:
             print(
-                f'overwritten titles mentions wrong default title. id={id1} default_title={default_title} overwritten_title_left={title1} translation={translation}')
+                f'Change table mentions wrong default title. id={id1} default_title={default_title} change_table_title={title1} translation={translation}')
+            sys.exit(1)
+
+
+def perform_title_check(id1, json_title, default_titles, overwritten_titles, translation):
+    if overwritten_titles is None or id1 not in overwritten_titles:
+        if id1 not in default_titles:
+            print(f'Translation contains unexpected id. id={id1} translation={translation}')
+            sys.exit(1)
+        default_title = default_titles[id1]
+        if json_title != default_title:
+            print(
+                f'Translation title is not equal to default title, but the change was not mentioned in change table. id={id1} translation_title={json_title} default_title={default_title} translation={translation}')
+            sys.exit(1)
+    else:
+        overwritten_title = overwritten_titles[id1][1]
+        if json_title != overwritten_title:
+            print(
+                f'Change table title and translation title are not equal. id={id1} change_table_title={overwritten_title} translation_title={json_title} translation={translation}')
             sys.exit(1)
 
 
@@ -71,39 +93,11 @@ def check_json_titles(json_loaded, default_titles, overwritten_titles, translati
     for p in json_loaded['parts']:
         part_title = p['part_title']
         id1, json_title = getIdAndTitle(part_title)
-        if overwritten_titles is None or id1 not in overwritten_titles:
-            if id1 not in default_titles:
-                print(f'json id is not found in default. id={id1} part_title={part_title} translation={translation}')
-                sys.exit(1)
-            default_title = default_titles[id1]
-            if json_title != default_title:
-                print(f'json title is not equal to default_title, but the change was not mentioned in TitleChangeTable{translation}.vue. id={id1} json_title={json_title} default_title={default_title} translation={translation}')
-                sys.exit(1)
-        else:
-            overwritten_title = overwritten_titles[id1][1]
-            if json_title != overwritten_title:
-                print(f'json title is not equal to overwritten_title. Part title contradicts TitleChangeTable{translation}.vue. id={id1} json_title={json_title} overwritten_title.={overwritten_title} translation={translation}')
-                sys.exit(1)
-        for s in  p['sections']:
+        perform_title_check(id1, json_title, default_titles, overwritten_titles, translation)
+        for s in p['sections']:
             id1 = s['id'] + '.'
             json_title = s['section_title']
-            if overwritten_titles is None or id1 not in overwritten_titles:
-                if id1 not in default_titles:
-                    print(
-                        f'json id is not found in default. id={id1} section_title={part_title} translation={translation}')
-                    sys.exit(1)
-                default_title = default_titles[id1]
-                if json_title != default_title:
-                    print(
-                        f'json title is not equal to default_title, but the change was not mentioned in TitleChangeTable{translation}.vue. id={id1} json_title={json_title} default_title={default_title} translation={translation}')
-                    sys.exit(1)
-            else:
-                overwritten_title = overwritten_titles[id1][1]
-                if json_title != overwritten_title:
-                    print(
-                        f'json_title is not equal to overwritten_title. Part title contradicts TitleChangeTable{translation}.vue. id={id1} json_title={json_title} overwritten_title.={overwritten_title} translation={translation}')
-                    sys.exit(1)
-
+            perform_title_check(id1, json_title, default_titles, overwritten_titles, translation)
 
 
 def main():
@@ -125,7 +119,7 @@ def main():
             translation = json_loaded['translation']
             if translation in tested_translations:
                 default_titles = titles_json[json_loaded['language']]
-                overwritten_titles = read_overwritten_titles(translation, components_folder)
+                overwritten_titles = read_overwritten_titles(components_folder, translation)
                 check_overwritten(default_titles, overwritten_titles, translation)
                 check_json_titles(json_loaded, default_titles, overwritten_titles, translation)
 
