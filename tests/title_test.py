1662be7945235e4d4d4f99ca322e456ea6a37211
Andrzej Polak <jjjkamu@gmail.com>
2024-09-11 19:29:55 +0200

    Calc overwritten json

--- tests/title_test.py
+++ tests/title_test.py
@@ -6,6 +6,11 @@ from bs4 import BeautifulSoup
 tested_translations = ['KG', 'SZIT', 'ESV']
 
 
+def getKeyAndTitle(table_element):
+    a, b = table_element.split(' ', 1)
+    return a, b
+
+
 def read_overwritten_titles(translation, components_folder):
     path = components_folder + 'TitleChangeTable' + translation + '.vue'
     if os.path.isfile(path) is False:
@@ -27,9 +32,24 @@ def read_overwritten_titles(translation, components_folder):
         if cols:
             cols = [col.string.strip() for col in cols]
             table_data.append(cols)
+    overwritten_titles = {}
     for row in table_data:
-        print(row)
-
+        if len(row) != 2:
+            print(f'Table row contains other than two elements. path={path} row={row}')
+            sys.exit(1)
+        id1, title1 = getKeyAndTitle(row[0])
+        id2, title2 = getKeyAndTitle(row[1])
+        if id1 != id2:
+            print(f'Table row contains different ids. id1={id1} id2={id2} path={path} row={row}')
+            sys.exit(1)
+        if title1 == title2:
+            print(f'Table row contains identical titles (superfluous row). title={title1} path={path} row={row}')
+            sys.exit(1)
+        if id1 in overwritten_titles:
+            print(f'Same Id is presented in multiple rows. id={id1} path={path}')
+            sys.exit(1)
+        overwritten_titles[id1] = [title1, title2]
+    return overwritten_titles
 
 def main():
     json_folder = sys.argv[1]
@@ -51,6 +71,7 @@ def main():
             if translation in tested_translations:
                 default_titles = titles_json[json_loaded['language']]
                 overwritten_titles = read_overwritten_titles(translation, components_folder)
+                x = 0
 
 
 if __name__ == "__main__":
