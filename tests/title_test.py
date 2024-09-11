e6349a96cf391983fa86413ce51d684e71287f47
Andrzej Polak <jjjkamu@gmail.com>
2024-09-11 18:39:54 +0200

    Add main

--- tests/title_test.py
+++ tests/title_test.py
@@ -11,16 +11,15 @@ with open(titles_json_file_path, 'r') as file:
     titles_json = json.load(file)
 
 
-def read_overwritten_titles(translation_str):
-    ret = None
-    path = components_folder + 'TitleChangeTable' + translation_str + '.vue'
+def read_overwritten_titles(translation):
+    path = components_folder + 'TitleChangeTable' + translation + '.vue'
     if os.path.isfile(path) is False:
-        return ret
-    with open(path, 'r', encoding='utf-8') as filee:
+        return None
+    with open(path, 'r', encoding='utf-8') as file:
         try:
-            vue_content = filee.read()
-        except ValueError as er:
-            print(f'Error while reading string from file={path}. error={er}')
+            vue_content = file.read()
+        except ValueError as e:
+            print(f'Error while reading string from file={path}. error={e}')
             sys.exit(1)
 
     soup = BeautifulSoup(vue_content, 'html.parser')
@@ -37,17 +36,22 @@ def read_overwritten_titles(translation_str):
         print(row)
 
 
-for file in os.listdir(os.fsencode(json_folder)):
-    filename = os.fsdecode(file)
-    if filename.endswith('.json'):
-        file_to_check = os.path.join(json_folder, filename)
-        with open(file_to_check) as f:
-            try:
-                json_loaded = json.load(f)
-            except ValueError as e:
-                print(f'Invalid json= {file_to_check}. error={e}')
-                sys.exit(1)
-        translation = json_loaded['translation']
-        if translation in tested_translations:
-            default_titles = titles_json[json_loaded['language']]
-            overwritten_titles = read_overwritten_titles(translation)
+def main():
+    for file in os.listdir(os.fsencode(json_folder)):
+        filename = os.fsdecode(file)
+        if filename.endswith('.json'):
+            file_to_check = os.path.join(json_folder, filename)
+            with open(file_to_check) as f:
+                try:
+                    json_loaded = json.load(f)
+                except ValueError as e:
+                    print(f'Invalid json= {file_to_check}. error={e}')
+                    sys.exit(1)
+            translation = json_loaded['translation']
+            if translation in tested_translations:
+                default_titles = titles_json[json_loaded['language']]
+                overwritten_titles = read_overwritten_titles(translation)
+
+
+if __name__ == "__main__":
+    main()
