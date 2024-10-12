fbdfd89616dc36bf0fd8e1e4241a94aeaf4be420
anwolosz <anwolosz@gmail.com>
2024-10-12 14:25:23 +0200

    Set distances in heading

--- src/views/SynopsisView.vue
+++ src/views/SynopsisView.vue
@@ -59,8 +59,8 @@ export default {
 <template>
   <Loader />
   <div class="container-fluid">
-    <h1 class="text-center display-1 pt-4">{{ synopsisStore.currentSynopsis.heading }}</h1>
-    <p class="text-center pb-4">{{ synopsisStore.currentSynopsis.subheading }}</p>
+    <h1 class="text-center display-1">{{ synopsisStore.currentSynopsis.heading }}</h1>
+    <p class="text-center">{{ synopsisStore.currentSynopsis.subheading }}</p>
 
 
     <template v-for="partIndex in synopsisStore.currentSynopsis.parts.length">
