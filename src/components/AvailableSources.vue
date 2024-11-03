55475c88c02220f906b537204078c7e2e7fca25a
anwolosz <anwolosz@gmail.com>
2024-11-03 19:09:12 +0100

    Scalable available sources

--- src/components/AvailableSources.vue
+++ src/components/AvailableSources.vue
@@ -1,13 +1,23 @@
+<script lang="ts">
+import { useSynopsisStore } from "@/stores/SynopsisStore"
+
+export default {
+    data() {
+        return {
+            synopsisStore: useSynopsisStore()
+        };
+    }
+}
+
+</script>
+
 <template>
-    <ul>    
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/kg.json" target="_blank">KG</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/szit.json" target="_blank">SZIT</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/knb.json" target="_blank">KNB</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/ruf.json" target="_blank">RUF</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/esv.json" target="_blank">ESV</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/bt.json" target="_blank">BT</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/bjw.json" target="_blank">BJW</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/sblgnt.json" target="_blank">SBLGNT</a></li>
-        <li><a href="https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/nv.json" target="_blank">NV</a></li>
-    </ul> 
+    <ul>
+        <li v-for="synopsis in synopsisStore.synopses">
+            <a
+            :href="`https://raw.githubusercontent.com/anwolosz/synopsis/main/src/assets/translations/${synopsis.translation.toLowerCase()}.json`">
+            {{synopsis.translation}}
+            </a>
+        </li>
+    </ul>
 </template>
