6ce5c64d5eb6f7c95b240c187e68099ce45aca15
janowol <jjjkamu@gmail.com>
2024-11-07 23:08:10 +0100

    Improve comments

--- tests/bible_ref.py
+++ tests/bible_ref.py
@@ -6,8 +6,24 @@ from file_utils import iterate_jsons
 
 @total_ordering
 class BibleRef:
+    """Represents a gospel reference with evangelist, chapter, verse, and optional notes, e.g., mt5,38 or jn19,16a"""
     bible_ref_list: list['BibleRef'] = []
     bible_ref_dict: dict['BibleRef', int] = {}
+
+    @classmethod
+    def class_init(cls, json_folder_path: str):
+        """Initialize static members; class_init must be called at the start of the script"""
+        if not cls.bible_ref_dict:
+            bible, _ = next(iterate_jsons(json_folder_path))
+            BibleRef._get_bible_refs(bible)
+
+            # add end element
+            a = BibleRef.bible_ref_list[-1]
+            BibleRef.bible_ref_list.append(BibleRef(a.evangelist, 99, 99, ''))
+
+            # init bible_ref_dict
+            for idx, ref in enumerate(cls.bible_ref_list):
+                cls.bible_ref_dict[ref] = idx
 
     def __init__(self, evangelist: str, chapter: int, verse: int, x: str):
         self.evangelist = evangelist
@@ -27,22 +43,8 @@ class BibleRef:
         idx2 = other._get_idx()
         return idx1 < idx2
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.evangelist, self.chapter, self.verse, self.x))
-
-    @classmethod
-    def class_init(cls, json_folder_path: str):
-        if not cls.bible_ref_dict:
-            bible, _ = next(iterate_jsons(json_folder_path))
-            BibleRef._get_bible_refs(bible)
-
-            # add end element
-            a = BibleRef.bible_ref_list[-1]
-            BibleRef.bible_ref_list.append(BibleRef(a.evangelist, 99, 99, ''))
-
-            # init bible_ref_dict
-            for idx, ref in enumerate(cls.bible_ref_list):
-                cls.bible_ref_dict[ref] = idx
 
     def next(self) -> 'BibleRef':
         idx = self._get_idx() + 1
@@ -53,6 +55,7 @@ class BibleRef:
 
     @staticmethod
     def from_string(repr_str: str) -> 'BibleRef':
+        """Parses a reference string in the form 'mt5,38' or 'jn19,16b'"""
         parts = repr_str.split(",")
         evangelist = parts[0][0:2]
         chapter = int(parts[0][2:])
@@ -62,6 +65,7 @@ class BibleRef:
 
     @staticmethod
     def split_verse(verse_str: str) -> Tuple[int, str]:
+        """Splits '16' into [16, ''] and '16b' into [16, 'b']"""
         if verse_str[-1].isdigit():
             verse = int(verse_str)
             x = ''
@@ -93,6 +97,7 @@ class BibleRef:
 
 
 class BibleSec:
+    """Represents consecutive gospel verses as [start, end), with start and end as BibleRef objects"""
     start: BibleRef
     end: BibleRef
 
@@ -113,6 +118,7 @@ class BibleSec:
         return self.end <= self.start
 
     def fix_closed(self):
+        """If end is inclusive, adjusts self to be a left-closed, right-open interval"""
         self.end = self.end.next()
 
     def intersect(self, other: 'BibleSec') -> 'BibleSec':
@@ -127,6 +133,9 @@ class BibleSec:
 
     @classmethod
     def from_closed_string(cls, closed_str: str) -> 'BibleSec':
+        """ 'lk5,5'->[lk5,5;lk5,6), 'mk9,43-48'->[mk9,43;mk9,49), 'mk9,43-50'->[mk9,43;mk10,1),
+            'mk9,43-10,2'->[mk9,43;mk10,3), 'mk1,1-6,6a'->[mk1,1;mk6,6b), 'mt8,11-mk6,6b'->[mt8,11;mk6,7),
+            'jn1,1-mk6,6'->[jn1,1;mk6,7), 'mt8,11-lk24,53'->[mt8,11;jn1,1), 'mt1,1-jn21,25'->[mt1,1;jn99,99) """
         if '-' in closed_str:
             str_1 = closed_str.split('-', 1)[0]
             start = BibleRef.from_string(str_1)
