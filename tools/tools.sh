6a6daf77ed52f0c1b1afa1a7ac49e2e585495050
Andrzej Polak <jjjkamu@gmail.com>
2024-11-08 11:07:48 +0100

    Fix tools

---
+++ tools/tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+TOOLS_FOLDER=$(dirname "${0}")
+WORKSPACE_FOLDER=$(readlink -e "${TOOLS_FOLDER}/../")
+TRANSLATION_FOLDER="$WORKSPACE_FOLDER/src/assets/translations/"
+
+python3 "$TOOLS_FOLDER"/bible_parser.py "$TRANSLATION_FOLDER" "$TOOLS_FOLDER"/bible_parser_input.txt bt bjw_init
+python3 "$TOOLS_FOLDER"/title_compare.py "$TRANSLATION_FOLDER"
+python3 "$TOOLS_FOLDER"/title_override.py "$TRANSLATION_FOLDER" "$TOOLS_FOLDER"/title_override_input.txt rsp
+echo "All tools ran"
