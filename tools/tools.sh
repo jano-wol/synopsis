57ac6ed51ac8041ea9e99a8c33a9377579c8ad2e
Andrzej Polak <jjjkamu@gmail.com>
2024-11-08 12:02:36 +0100

    Improve tools.sh

--- tools/tools.sh
+++ tools/tools.sh
@@ -7,4 +7,5 @@ TRANSLATION_FOLDER="$WORKSPACE_FOLDER/src/assets/translations/"
 python3 "$TOOLS_FOLDER"/bible_parser.py "$TRANSLATION_FOLDER" "$TOOLS_FOLDER"/bible_parser_input.txt bt bjw_init
 python3 "$TOOLS_FOLDER"/title_compare.py "$TRANSLATION_FOLDER"
 python3 "$TOOLS_FOLDER"/title_override.py "$TRANSLATION_FOLDER" "$TOOLS_FOLDER"/title_override_input.txt rsp
+python3 "$TOOLS_FOLDER"/dump_texts.py "$TRANSLATION_FOLDER" nv
 echo "All tools ran"
