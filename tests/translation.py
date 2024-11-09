8d4955039627c3d3beb4b0e830ddc80dcd849f91
janowol <jjjkamu@gmail.com>
2024-11-09 18:57:55 +0100

    Fill section_titles

--- tests/translation.py
+++ tests/translation.py
@@ -8,15 +8,15 @@ from bible import BibleRef, BibleSec, evangelists
 class BoxRef:
     """Represents a box reference in a translation"""
 
-    def __init__(self, part_idx: int, section_idx_loc: int, section_idx_glob: int, e: int, idx: int):
+    def __init__(self, part_idx: int, section_idx_loc: int, section_id: int, e: int, idx: int):
         self.part_idx = part_idx
         self.section_idx_loc = section_idx_loc
-        self.section_idx_glob = section_idx_glob
+        self.section_id = section_id
         self.e = e
         self.idx = idx
 
     def __repr__(self) -> str:
-        return f'({self.part_idx};{self.section_idx_glob};{evangelists[self.e]};{self.idx})'
+        return f'({self.part_idx};{self.section_id};{evangelists[self.e]};{self.idx})'
 
 
 class Box:
@@ -44,10 +44,21 @@ class Translation:
     def __init__(self, loaded_json):
         self.json: json = loaded_json
         self.part_titles: list[str] = []
-        self.section_titles: list[str] = []
+        self.section_titles: dict[str, str] = {}
         self.ref_to_text: dict[BibleRef, str] = {}
         self.body_ref_to_box_ref: dict[BibleRef, BoxRef] = {}
         self.body_text_partition: list[BibleSec] = []
+
+        for part in self.iterate_on_parts():
+            self.part_titles.append(part['part_title'])
+
+        for section in self.iterate_on_sections():
+            id_str = section['id']
+            assert id_str.isdigit()
+            assert id_str not in self.section_titles
+            self.section_titles[id_str] = section['section_title']
+
+
 
     def __repr__(self) -> str:
         return str(self.json)
@@ -72,6 +83,7 @@ class Translation:
         for box in self.iterate_on_boxes():
             if box and box['leading']:
                 yield box
+
 
     def get_box(self, ref: BoxRef) -> Box:
         return Box(self.json['parts'][ref.part_idx]['sections'][ref.section_idx_loc][evangelists[ref.e]][ref.idx], ref)
