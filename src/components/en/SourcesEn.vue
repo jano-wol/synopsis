0a0727b4970c73ae33b9f384c24b8179957974d6
anwolosz <anwolosz@gmail.com>
2024-11-11 18:51:44 +0100

    Move Differences from the sources to wiki

--- src/components/en/SourcesEn.vue
+++ src/components/en/SourcesEn.vue
@@ -14,63 +14,8 @@ export default {
 <template>
     <h2 class="fs-3">Sources used</h2>
     <SourcesUsed />    
-    <h2 class="fs-3">Differences compared to sources</h2>
-    <ul>
-        <li>
-            Secondary parallels are not included.<br>
-            Reason: Simplification.<br>
-        </li>
-        <br>
-        <li>
-            With the exception of quotations containing the main text, the whole verse is given instead of the
-            sub-verses.<br>
-            Reason: Simplification.<br>
-            Note: The continuity of the main text is preserved.
-        </li>
-        <br>
-        <li>
-            Sections 358, 359 and 360 are not included.<br>
-            Reason: These sections do not contain body text.<br>
-            Note: The numbering of other parts remains the same.
-        </li>
-        <li>
-            Section 361 is not included.<br>
-            Reason: It refers to 1Cor15,3-8, which is not part of the four gospels.<br>
-            Note: The numbering of other parts remains the same.
-        </li>
-        <li>
-            Section 362 is not included.<br>
-            Reason: It follows the non-canonical ending of Mark.<br>
-            Note: The numbering of other parts remains the same.
-        </li>
-        <br>
-        <!-- TODO: Esetleg Jn6,6 és Jn6,67-71 szétvágás is lehetséges-->
-        <li>
-            In section 365, the quotation Mk16,15.19 has been separated to Mk16,15 and Mk16,19 .<br>
-            Reason: Simplification.
-        </li>
-        <br>
-        <li>
-            In SZIT translation:<br>
-            The quotation Mk6,6a in Synopsis is equivalent with quotation Mk6,6 in SZIT edition.<br>
-            The quotation Mk6,6b in Synopsis is equivalent with beginning of quotation Mk6,7 in SZIT edition.<br>
-            Reason: No exceptional numbering follows the main text of the Synopsis compared to other translations.<br>
-            Note: Probably an error in the editing of the text of the SZIT publisher.
-        </li>
-        <li>
-            In BT translation:<br>
-            In section 229, the quotation Lk17,3a is equivalent with ending of quotation Lk17,2 in BT edition.<br>
-            In section 230, the quotation Lk17,3b is equivalent with quotation Lk17,3 in BT edition.<br>
-            Reason: No exceptional numbering follows the main text of the Synopsis compared to other translations.<br>
-            Note: Probably an error in the editing of the text of the BT publisher.
-        </li>
-        <br>
-        <li>
-            The structural names of Synopsis have been modified in some translations.<br>
-            Reason: Unification, better readability, etc.<br>
-            <!-- Note: For details of the unification, see the documentation for contributors. -->
-        </li>
-    </ul>
+    <h2 class="fs-3">Differences from the sources</h2>
+    <p>The implementation differs from the sources, with significant differences in some cases. These differences are documented on the project's <a href="https://github.com/anwolosz/synopsis/wiki/Differences-from-the-sources">wiki page</a>.</p>
     <h2 class="fs-3">Available sources</h2>
     <AvailableSources />
 </template>
--- src/components/hu/SourcesHu.vue
+++ src/components/hu/SourcesHu.vue
@@ -15,61 +15,7 @@ export default {
     <h2 class="fs-3">Felhasznált források</h2>
     <SourcesUsed />    
     <h2 class="fs-3">Eltérések a forrásoktól</h2>
-    <ul>
-        <li>
-            A másodlagos párhuzamok nem szerepelnek.<br>
-            Indoklás: Egyszerűsítés.<br>
-        </li>
-        <br>
-        <li>
-            A törzsszöveget tartalmazó idézetek kivételével az alversek helyett mindig az egész vers szerepel.<br>
-            Indoklás: Egyszerűsítés.<br>
-            Megjegyzés: A törzsszöveg folytonossága így fennmarad.
-        </li>
-        <br>
-        <li>
-            A 358., 359., 360. szekció nem szerepel.<br>
-            Indoklás: Ezek a szekciók nem tartalmaznak törzsszöveget.<br>
-            Megjegyzés: A további fejezetek számozása nem változik.
-        </li>
-        <li>
-            A 361. szekció nem szerepel.<br>
-            Indoklás: 1Kor15,3-8 fejezetre hivatkozik, amely nem része a négy evangéliumnak.<br>
-            Megjegyzés: A további fejezetek számozása nem változik.
-        </li>
-        <li>
-            A 362. szekció nem szerepel.<br>
-            Indoklás: Márk nem kanonikus befejezését követi.<br>
-            Megjegyzés: A további fejezetek számozása nem változik.
-        </li>
-        <br>
-        <!-- TODO: Esetleg Jn6,6 és Jn6,67-71 szétvágás is lehetséges-->
-        <li>
-            A 365. szekcióban a Mk16,15.19 idézet Mk16,15 és Mk16,19 idézetekre lett szétvágva.<br>
-            Indoklás: Egyszerűsítés.
-        </li>
-        <br>
-        <li>
-            A SZIT fordításában:<br>
-            A Szinopszis Mk6,6a idézete a SZIT szerkesztésében a Mk6,6 idézetnek felel meg.<br>
-            A Szinopszis Mk6,6b idézete a SZIT szerkesztésében a Mk6,7 idézet elejének felel meg.<br>
-            Indoklás: Nem követi kivételes számozás a Szinopszis törzsszövegét a többi fordításhoz képest.<br>
-            Megjegyzés: Feltehetően hiba a Szent István Társulat szövegének szerkesztésében.
-        </li>
-        <li>
-            Az BT fordításában:<br>
-            A 229. szekcióban a Lk17,3a idézet az BT szerkesztésében a Lk17,2 idézet végének felel meg.<br>
-            A 230. szekcióban a Lk17,3b idézet az BT szerkesztésében a Lk17,3 idézetnek felel meg.<br>
-            Indoklás: Nem követi kivételes számozás a Szinopszis törzsszövegét a többi fordításhoz képest.<br>
-            Megjegyzés: Feltehetően hiba a Biblia Tysiąclecia szövegének szerkesztésében.
-        </li>
-        <br>
-        <li>
-            A Szinopszis szerkezeti elnevezései néhány fordításban módosítva lettek.<br>
-            Indoklás: Egységesítés a Biblia szöveggel, olvashatóság javítása, stb.<br>
-            <!-- Megjegyzés: Az egységesítés szempontjait lásd a contributorok számára készített dokumentációban.  -->
-        </li>
-    </ul>
+    <p>A megvalósítás (bizonyos esetekben jelentősen) eltér a forrásoktól. Ezek a különbségek a projekt <a href="https://github.com/anwolosz/synopsis/wiki/Differences-from-the-sources">wiki oldalán</a> találhatók felsorolva.</p>
     <h2 class="fs-3">Felhasználható források</h2>
     <AvailableSources />
 </template>
