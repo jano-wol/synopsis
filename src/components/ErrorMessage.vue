d75089fea1b181676d130d4bbcd3e0f65d1cb511
anwolosz <anwolosz@gmail.com>
2024-11-23 21:11:03 +0100

    Error message translations

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -13,9 +13,9 @@ export default {
         getMessage(error : ErrorMessageEnum) {
         switch (error) {
             case ErrorMessageEnum.SERVER:
-                return 'The requested gospel is currently unavailable.';
+                return this.synopsisStore.currentDictionary.error.server;
             case ErrorMessageEnum.DATE:
-                return `Invalid date (${this.$route.params.date}). Please use the format YYYY-MM-DD (e.g., ${new Date().toISOString().split('T')[0]}).`;
+                return this.synopsisStore.currentDictionary.error.date;
         }
         }
     }
