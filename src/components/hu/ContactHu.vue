517856deb1690c5803afc511078138218d88d17b
janowol <jjjkamu@gmail.com>
2024-12-01 14:16:15 +0100

    Try to finalize Hungarian

--- src/components/hu/ContactHu.vue
+++ src/components/hu/ContactHu.vue
@@ -2,22 +2,22 @@
     <h2 class="fs-3">
         E-mail
     </h2>
-    <p>Bármilyen észrevétel, kérdés vagy javaslat esetén várom az üzeneteket az alábbi címen:</p>
-    <p><b>contact@synopticus.org</b></p>
+    <p>Bármilyen észrevétel, kérdés vagy javaslat esetén kérjük, vegye fel velünk a kapcsolatot az alábbi címen:<br>
+    <b>contact@synopticus.org</b>
+    </p>
     <h2 class="fs-3">
         Wiki
     </h2>
     <p>
-        A projekthez tartozik egy <a href="https://github.com/anwolosz/synopsis/wiki">Wiki</a> oldal, amely a fejlesztés során hozott döntéseket, kérdéseket, észrevételeket dokumentálja. Az errata szekcióban a felhasználók által észrevett hibák találhatók. 
+        A projekthez tartozik egy <a href="https://github.com/anwolosz/synopsis/wiki">wiki oldal</a>, amely a fejlesztés során hozott döntéseket, kérdéseket, észrevételeket dokumentálja. Az <a href="https://github.com/anwolosz/synopsis/wiki/Errata">Errata</a> szekcióban a felhasználók által észrevett hibák találhatók.
     </p>
     <h2 class="fs-3">
         Fejlesztés
     </h2>
-    <p>A <a href="https://www.synopticus.org/">synopticus.org</a> nyíltforráskódú projekt. A fejlesztés GitHubon zajlik, a repository <a
+    <p>A <a href="https://www.synopticus.org/">synopticus.org</a> nyílt forráskódú projekt. A fejlesztés GitHubon zajlik, a repository <a
             href="https://github.com/anwolosz/synopsis" target="_blank">ezen</a> a
-        linken érhető el.
-    </p>
-    <p>Jelenleg a különböző evangélium fordítások integrációja a cél. A Szinopszis struktúrájában eddig feldolgozott
-        fordtások <code class="text-dark">.json</code> formátumban elérhetők a <router-link :to="{name: 'sources'}">Források</router-link> menü pontban a Letölthető anyagok cím alatt.
+        linken érhető el.<br>
+    Jelenleg a különböző evangéliumfordítások integrációja a cél. A Szinopszis struktúrájában eddig feldolgozott
+        fordtások <code class="text-dark">.json</code> formátumban elérhetők a <router-link :to="{name: 'sources'}">Források</router-link> menüpontban a Letölthető anyagok cím alatt.
     </p>
 </template>
