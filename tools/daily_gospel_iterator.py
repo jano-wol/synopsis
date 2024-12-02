9b853d0b1f6cd9f6b85a9efcdb7fe6cdb7bf490b
Andrzej Polak <jjjkamu@gmail.com>
2024-12-02 17:30:37 +0100

    Improve script

--- tools/daily_gospel_iterator.py
+++ tools/daily_gospel_iterator.py
@@ -1,6 +1,13 @@
 import json
 import sys
 
+from bible import BibleRef
+
+rename = {'mt': 0,
+          'mk': 1,
+          'lk': 2,
+          'jn': 3}
+
 
 def main():
     start_json_path = sys.argv[1]
@@ -16,19 +23,61 @@ def main():
             colours.add(c)
     print(colours)
 
-    broken_verses_start = set()
-    broken_verses_end = set()
+    sub_ref_problems = set()
     for a in start_json:
         assert len(start_json[a]['daily_gospel']) > 0
         dg = start_json[a]['daily_gospel']
         for reading in dg:
             for interval in reading['intervals']:
                 if not interval['start']['verse'].isdigit():
-                    broken_verses_start.add(reading['evangelist'] + '_' + str(interval['start']['chapter']) + '_' + interval['start']['verse'])
+                    my_string = interval['start']['verse']
+                    rest = my_string[:-1]
+                    last_char = my_string[-1]
+                    assert rest.isdigit()
+                    assert last_char == 'b'
+                    sub_ref_problems.add(BibleRef(rename[reading['evangelist']], int(interval['start']['chapter']), int(rest), 1))
                 if not interval['end']['verse'].isdigit():
-                    broken_verses_end.add(reading['evangelist'] + '_' + str(interval['end']['chapter']) + '_' + interval['end']['verse'])
-    print(broken_verses_start)
-    print(broken_verses_end)
+                    my_string = interval['end']['verse']
+                    rest = my_string[:-1]
+                    last_char = my_string[-1]
+                    assert rest.isdigit()
+                    assert last_char == 'a'
+                    sub_ref_problems.add(BibleRef(rename[reading['evangelist']], int(interval['end']['chapter']), int(rest), 2))
+    all_refs = set()
+    curr = BibleRef.begin()
+    while curr != BibleRef.end():
+        all_refs.add(curr)
+        curr = curr.next()
+    print(len(all_refs))
+    for a in start_json:
+        assert len(start_json[a]['daily_gospel']) > 0
+        dg = start_json[a]['daily_gospel']
+        for reading in dg:
+            for interval in reading['intervals']:
+                assert interval['start']['verse'].isdigit()
+                start = BibleRef(rename[reading['evangelist']], int(interval['start']['chapter']),
+                             int(interval['start']['verse']), 0)
+                if interval['end']['verse'].isdigit():
+                    end = BibleRef(rename[reading['evangelist']], int(interval['end']['chapter']),
+                               int(interval['end']['verse']), 0)
+                else:
+                    my_string = interval['end']['verse']
+                    rest = my_string[:-1]
+                    last_char = my_string[-1]
+                    assert rest.isdigit()
+                    assert last_char == 'a'
+                    x = 1
+                    end = BibleRef(rename[reading['evangelist']], int(interval['end']['chapter']), int(rest), x)
+                while start <= end:
+                    n = BibleRef(start.e, start.chapter, start.verse, 2)
+                    if n in sub_ref_problems:
+                        if end.get_base_ref() != start:
+                            sub_ref_problems.discard(n)
+                    all_refs.discard(start)
+                    start = start.next()
+    print(f'not read because of verse brakes: {sorted(sub_ref_problems)}')
+    all_refs_sorted = sorted(all_refs)
+    print(f'not read because of missing: {sorted(all_refs_sorted)}')
 
 if __name__ == '__main__':
     main()
